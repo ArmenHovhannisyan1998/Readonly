7ebd82368aaa955550a06352b6717f0bfc55aa3a
ArmenHovhannisyan1998 <48031430+ArmenHovhannisyan1998@users.noreply.github.com>
2019-03-16 21:07:19 +0400

    Add project files.

---
+++ Readonly/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Readonly
+{
+    class Program
+    {
+        public readonly string field = "Hello ";
+
+        public Program()
+        {
+            field = "Real magic";
+            field += "!";
+        }
+
+        static void Main(string[] args)
+        {
+            Program instance = new Program();
+
+            Console.WriteLine(instance.field);
+
+            Console.ReadKey();
+        }
+    }
+}
